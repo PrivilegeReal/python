baf4de824a06c5fa122a9e1f95ad4c9446b29d65
Privilege <maltsev.a.n@yandex.ru>
2020-08-28 18:17:48 +0300

    Initial commit

---
+++ task 1.py
@@ -0,0 +1,14 @@
+# 1.Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран.
+
+number1 = 117
+number2 = 25
+print(number1, number2)
+
+user_answer1 = input('Введите число 1: ')
+user_answer2 = input('Ведите число 2: ')
+user_answer3 = str(input('Введите текст 1: '))
+user_answer4 = str(input('Введите текст 2: '))
+
+print(user_answer1, user_answer2, user_answer3, user_answer4)
